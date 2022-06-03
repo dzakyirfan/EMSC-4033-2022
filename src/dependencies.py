60ead97f197ba79b96371855bd26be5b9af26fc0
Dzaky Irfansyah <80967228+dzakyirfan@users.noreply.github.com>
2022-06-03 21:41:40 +1000

    Add src folder
    the src folder contains files that store dependencies, required functions, and test functions

---
+++ src/dependencies.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 19:54:53 2022
+
+@author: dzakyirfan
+"""
+
+"""Import necessary modules for basin project"""
+
+import numpy as np
+import os
+import glob
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.graph_objects as go
+
+from IPython.display import display_markdown
